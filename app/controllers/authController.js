eb9bc104d87e917f34a0540be2180e0f0842d655
ilyasyasin072 <ilyasyasiin072@github.com>
2021-03-25 14:32:49 +0700

    comment require

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
-var bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({ extended: false }));
-router.use(bodyParser.json());
+// var express = require('express');
+// var router = express.Router();
+// var bodyParser = require('body-parser');
+// router.use(bodyParser.urlencoded({ extended: false }));
+// router.use(bodyParser.json());
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var User = require('../../models/User')
